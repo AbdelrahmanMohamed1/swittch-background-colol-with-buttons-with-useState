421c364ca6acdcf95a789381c152f182d13ccee7
AbdelrahmanMohamed1 <abdelrhmanmohamed1010@gmail.com>
2023-08-22 20:17:48 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import "./App.css";
+import "./theme.css";
+import { useState } from "react";
+
+function App() {
+  const [person, setPerson] = useState("boody");
+  const [age, setage] = useState("26");
+  const [count, setcount] = useState(0);
+  const [theme, settheme] = useState("");
+
+  const changeName = () => {
+    setage("30");
+  };
+
+  return (
+    <div className={`App ${theme}`}>
+      <div>
+        <button
+          onClick={() => {
+            settheme("");
+          }}
+          style={{ marginRight: "26px" }}
+        >
+          Light
+        </button>
+        <button
+          onClick={() => {
+            settheme("dark");
+          }}
+          style={{ marginRight: "26px" }}
+        >
+          Dark
+        </button>
+        <button
+          onClick={() => {
+            settheme("grey");
+          }}
+          style={{ marginRight: "26px" }}
+        >
+          Grey
+        </button>
+        <button
+          onClick={() => {
+            settheme("pink");
+          }}
+        >
+          Pink
+        </button>
+      </div>
+
+      <h2 style={{ marginTop: "66px" }}>My name is {person}</h2>
+      <button
+        onClick={() => {
+          setPerson("ELRAYEK♣");
+        }}
+      >
+        Change name
+      </button>
+      <br />
+      <br />
+
+      <h2>My Age is {age}</h2>
+      <button onClick={changeName}>Change Age</button>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setcount(count + 10);
+        }}
+      >
+        count is {count}
+      </button>
+    </div>
+  );
+}
+
+export default App;
